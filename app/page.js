93ba2ae4e0c249053ca2bd74e8b6ced78e721cac
AhmedAbdelal57 <ahmed.m.abdelal57@gmail.com>
2025-01-20 16:38:58 +0200

    third commit

--- app/page.js
+++ app/page.js
@@ -6,6 +6,22 @@ import { useUser } from "@clerk/nextjs";
 import SignIn from "./(auth)/sign-in/_components/SignIn";
 
 export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <ProductSection />
+    </div>
+  );
+}
+
+/* "use client";
+import Image from "next/image";
+import HeroSection from "./_components/HeroSection";
+import ProductSection from "./_components/ProductSection";
+import { useUser } from "@clerk/nextjs";
+import SignIn from "./(auth)/sign-in/_components/SignIn";
+
+export default function Home() {
   const { user } = useUser();
   return user ? (
     <div>
@@ -17,4 +33,4 @@ export default function Home() {
       <SignIn />
     </div>
   );
-}
+} */
